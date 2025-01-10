951487f2a0c6e1098e8564ee014d3ad31e3a87a1
el160 <ljhongeri04@gmail.com>
2025-01-10 19:50:28 +0300

    updates

--- myapp/forecast/weatherapp/views.py
+++ myapp/forecast/weatherapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+import datetime
+
 # Create your views here.
+
+
+def home (request):
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'Mombasa'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={'Mombasa'}&appid=2933b7b19ca8a87bdb67e21a091ffd5b' 
+    PARAMS = {'units':'metric'}
+    
+    data = request.get(url,PARAMS).json() 
+    
+    description = data['weather'][0]['description'] 
+    icon = data['weather'][0]['icon'] 
+    temp = data['main']['temp']
+    
+    day = datetime.date.today()
+    
+    
+    return render(request,'weatherapp/home.html',{'desription':description,'icon':temp,'day':day})
+    
