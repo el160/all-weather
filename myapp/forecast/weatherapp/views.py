072bfce6cfd901f89692bd626ab5df313f55cefb
el160 <ljhongeri04@gmail.com>
2025-01-18 18:40:30 +0300

    updates

--- myapp/forecast/weatherapp/views.py
+++ myapp/forecast/weatherapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests #  library used to make HTTP requests
 import datetime #  helps us work with dates
+from django import messages
 
 # Create your views here.
 
@@ -16,18 +17,28 @@ def home (request):
     
     #additional parameters we send to the API, e.g., units in Celsius
     PARAMS = {'units':'metric'}
-    ## Use the 'requests' library to fetch data from the OpenWeather API
-    data = requests.get(url,PARAMS).json() #method to make HTTP requests
+    
+    try:
+        ## Use the 'requests' library to fetch data from the OpenWeather API
+        data = requests.get(url,PARAMS).json() #method to make HTTP requests
     
     #Extract necessary information from the JSON response
-    description = data['weather'][0]['description'] # Weather description (e.g., clear sky)
-    icon = data['weather'][0]['icon']# Weather icon code
-    temp = data['main']['temp'] # Current temperature in Celsius
+        description = data['weather'][0]['description'] # Weather description (e.g., clear sky)
+        icon = data['weather'][0]['icon']# Weather icon code
+        temp = data['main']['temp'] # Current temperature in Celsius
     
-    #get today's date
-    day = datetime.date.today()
+    #get today's date 
+        day = datetime.date.today()
     
     #render the template with data
-    return render(request,'weatherapp/home.html',{
-        'description':description,'icon':icon ,'temp':temp,'day':day,'city':city})
+        return render(request,'weatherapp/home.html',{
+        'description':'clear sky','icon':'01d' ,'temp':25,'day':day,'city':'Mombasa','exception_occured':False})
+    except:
+        exception_occured=True
+        messages.error(request,'Entered info is not available to API')
+        day=datetime.date.today()
+        
+        return render(request,'weatherapp/home.html',{
+        'description':'clear sky','icon':'01d' ,'temp':25,'day':day,'city':'Mombasa','exception_occured':False,})
+    
     
